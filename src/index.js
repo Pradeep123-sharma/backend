14dbcc23fa6ebccdeb255f42e58a9ad26d28673d
Pradeep123-sharma <pradeep131006@gmail.com>
2025-04-07 14:14:28 +0530

    connect to mongoDB atlas

---
+++ src/index.js
@@ -0,0 +1,68 @@
+import dotenv from "dotenv"
+import connectDB from "./db/dbconnect.js";
+dotenv.config({
+    path: './.env'
+})
+
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* import express from "express";
+const app = express();
+
+Now generally if we use function then we write it as:
+            function connectDB(){}
+            connectDB();
+Now this approach is better but better than this is below by using iife.
+
+sometimes before using iife in professional work some use semicolon before it we do not put semicolon in the above line then problem can happen.
+
+( async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)//to connect we need connection string and db name
+        
+        //Now generraly if we import our app then sometimes app cannot interact with db,then
+        app.on("error", (error)=>{
+            console.log("Error:",error);
+            throw error;
+        })
+        
+        //If app is able to interact with db
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.log("Error:", error);
+        throw error;
+    }
+})() */
